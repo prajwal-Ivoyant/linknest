f266f11a0b422f9068e6cee736968b76eed2f41a
prajwal-Ivoyant <prajwalp@ivoyant.com>
2026-03-26 12:46:03 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,11 +15,19 @@ connectDB();
 
 // Security middleware
 app.use(helmet());
+// app.use(cors({
+//   origin: process.env.CLIENT_URL ,//|| 'http://localhost:5173'
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
+
 app.use(cors({
-  origin: process.env.CLIENT_URL ,//|| 'http://localhost:5173'
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: [
+    "http://localhost:5173",
+    "https://linknest-ai-bookmarkmanager.netlify.app"
+  ],
+  credentials: true
 }));
 
 // Rate limiting
